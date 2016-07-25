2eae8f99a72b29f41bd03847503bd2d8e58e0165
Jan-Willem de Bruyn <emailofmeself+Github@gmail.com>
2016-07-25 16:04:18 +0200

    Update map.js

--- static/map.js
+++ static/map.js
@@ -640,9 +640,11 @@ function processPokestops(i, item) {
     else {
         item2 = map_data.pokestops[item.pokestop_id];
         if (!!item.lure_expiration != !!item2.lure_expiration || item.active_pokemon_id != item2.active_pokemon_id) {
+        if (item.marker != null) {
             item.marker.setMap(null);
             item.marker = setupPokestopMarker(item);
             map_data.pokestops[item.pokestop_id] = item;
+        }
         }
     }
 }

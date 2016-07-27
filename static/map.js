7b834c7bdeaac04d920ca239385277acc35eb49b
Jan-Willem de Bruyn <emailofmeself+Github@gmail.com>
2016-07-27 18:12:54 +0200

    Update map.js trying to get freaking performance

--- static/map.js
+++ static/map.js
@@ -18,6 +18,7 @@ var rawDataIsLoading = false;
 var locationMarker;
 var marker;
 var timeout;
+var timeout2;
 
 var noLabelsStyle=[{featureType:"poi",elementType:"labels",stylers:[{visibility:"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]}];
 var light2Style=[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
@@ -864,7 +865,9 @@ function processScanned(i, item) {
 
 function updateMap() {
 
-    loadRawData().done(function (result) {
+  window.clearTimeout(timeout);
+timeout = window.setTimeout(function () {
+  loadRawData().done(function (result) {
         $.each(result.pokemons, processPokemons);
         $.each(result.pokestops, processPokestops);
         $.each(result.pokestops, processLuredPokemon);
@@ -877,6 +880,9 @@ function updateMap() {
         clearOutOfBoundsMarkers(map_data.scanned);
         clearStaleMarkers();
     });
+
+    }, 500);
+});
 };
 
 

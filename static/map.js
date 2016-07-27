2f14632a97fdcbbaf191259af7eb60f91ffd16c9
Jan-Willem de Bruyn <emailofmeself+Github@gmail.com>
2016-07-27 18:08:12 +0200

    Update map.js trying to get freaking performance

--- static/map.js
+++ static/map.js
@@ -17,6 +17,7 @@ var map;
 var rawDataIsLoading = false;
 var locationMarker;
 var marker;
+var timeout;
 
 var noLabelsStyle=[{featureType:"poi",elementType:"labels",stylers:[{visibility:"off"}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]}];
 var light2Style=[{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}];
@@ -272,10 +273,17 @@ function initMap() {
         updateMap();
     });
 
-    google.maps.event.addListener(map, 'zoom_changed', function() {
-        redrawPokemon(map_data.pokemons);
+google.maps.event.addListener(map, 'zoom_changed', function () {
+window.clearTimeout(timeout);
+timeout = window.setTimeout(function () {
+     redrawPokemon(map_data.pokemons);
         redrawPokemon(map_data.lure_pokemons);
-    });
+
+    }, 500);
+}); //time in ms, that will reset if next 'bounds_changed' call is send, otherwise code will be executed after that time is up
+   // google.maps.event.addListener(map, 'zoom_changed', function() {
+      
+  //  });
 };
 
 function createSearchMarker() {
